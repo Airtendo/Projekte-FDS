266e62a921bde9d4a98a9a411880c5feadb76d67
Airtendo <joshua@himberger.xyz>
2023-10-16 09:12:43 +0200

    Create HelloWorld.java

---
+++ Hello World!/HelloWorld.java
@@ -0,0 +1,7 @@
+package Schule.LF05.HelloWorld; //Speicherort von Java-File definieren
+public class HelloWorld {       //HelloWorld Klasse erstellen
+    public static void main(String[] args) { 
+        String HelloWorld = "Hello World!"; //Variable 'HelloWorld' definieren
+        System.out.println(HelloWorld);     //Variable 'HelloWorld' ausgeben
+    }
+}
